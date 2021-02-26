8961982dc83acdcc85a389ec19f7b7fd4b6f6c9e
Luiz Felipe Marinho <78932869+Felipemarinho18@users.noreply.github.com>
2021-02-26 16:25:16 -0300

    Add files via upload

---
+++ AULA001.java
@@ -0,0 +1,9 @@
+package aula001;
+
+class AULA001{
+public static void main (String[] args){
+
+System.out.println("Tudo Bem?");System.out.println("Vai ficar");
+System.out.println("Vai ficar");
+ } 
+}
