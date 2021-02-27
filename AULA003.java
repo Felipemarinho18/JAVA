c0dc52f2a1d7145de37eb449951387a8afb778d1
Luiz Felipe Marinho <78932869+Felipemarinho18@users.noreply.github.com>
2021-02-27 10:27:12 -0300

    Add files via upload

---
+++ AULA003.java
@@ -0,0 +1,15 @@
+// Somando variaveis do tipo inteiro
+package aulas;
+
+
+public class AULA003 {
+    public static void main(String[]args){
+    int x = 2;
+    int y = 6;
+    int soma = x + y;
+    
+    System.out.println(x+" + "+y+" = "+soma);
+    
+    }
+    
+}
