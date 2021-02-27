ba240e13a8a2f96d43580b1d45e929ceb7632dc0
Luiz Felipe Marinho <78932869+Felipemarinho18@users.noreply.github.com>
2021-02-27 11:21:30 -0300

    Add files via upload

---
+++ AULA006.java
@@ -0,0 +1,17 @@
+//Criando um sistema que calcula a area de um retangulo
+package aulas;
+
+
+public class AULA006 {
+    public static void main (String[]args){
+    
+    double base, altura, area_retangulo;
+    base = 10.34;
+    altura = 20;
+    area_retangulo = base * altura;
+    
+    System.out.println("A area de um retangulo de base "+base+" e altura "+altura+" é igual a: ");
+    System.out.println(area_retangulo);
+    }
+    
+}
