c5e78c87cbc285e01c07e6780579fafa65bc3e8e
Luiz Felipe Marinho <78932869+Felipemarinho18@users.noreply.github.com>
2021-02-27 10:41:10 -0300

    Add files via upload

---
+++ AULA004.java
@@ -0,0 +1,16 @@
+// Criando e multiplicando variaveis do tipo double 
+package aulas;
+
+
+public class AULA004 {
+    public static void main(String[]args){
+    
+    double x = 8.5;
+    double y = 5.5;
+    double resultado = x * y;
+    
+    System.out.println(x+" * "+y+" = "+resultado);
+    
+    }
+    
+}
