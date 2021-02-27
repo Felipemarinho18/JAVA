4399260f6cabdda7a80ef947e9a600eb6fdf4a1e
Luiz Felipe Marinho <78932869+Felipemarinho18@users.noreply.github.com>
2021-02-27 10:14:02 -0300

    Add files via upload

---
+++ AULA002.java
@@ -0,0 +1,13 @@
+//Criando variaveis do tipo inteiro 
+package aulas;
+
+public class AULA002 {
+    public static void main(String[]args){
+        
+        int x;
+        x=20;
+        System.out.println("Você tem " + x + " anos de idade.");
+        
+    }
+    
+}
