f9bafcb7a29c94f35e7c647ea9834045bc3901b5
Luiz Felipe Marinho <78932869+Felipemarinho18@users.noreply.github.com>
2021-02-27 11:01:07 -0300

    Add files via upload

---
+++ AULA005.java
@@ -0,0 +1,22 @@
+// Tipos de dados primitivos 
+package aulas;
+
+public class AULA005 {
+    public static void main(String[]args){
+    
+    byte a; //Armazena dados com valores pequenos < 8bts(127)
+    short b;//Armazena dados com valores < 16bts (32.767)
+    int c;//Armazena dados com valores < 32bts (2bi)
+    long d;//Armazena dados com valoes grandes
+    
+    
+    float e;//Armazena valores decimais
+    double f;//Armazena valores decimais com mais precisão
+    
+    
+    boolean g;//Armazena dois tipos de valores(0 ou 1), (F ou V), (M ou F)
+    char h;//Armazena caracteres usando ex: h = "5"
+    
+    }
+    
+}
