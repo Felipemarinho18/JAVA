93bc1961af20d9d18d434a39521afc1b70cb93f8
Luiz Felipe Marinho <78932869+Felipemarinho18@users.noreply.github.com>
2021-02-27 16:29:15 -0300

    Add files via upload

---
+++ AULA008.java
@@ -0,0 +1,26 @@
+// Variavel constante e calculando area de um circulo
+package aulas;
+
+import java.util.Scanner;
+
+
+public class AULA008 {
+    public static void main(String[]args){
+    
+    final double PI = 3.1456;
+    Scanner input = new Scanner(System.in);
+    
+    System.out.println("Calculando a area de um Circulo\n\n");
+    System.out.println("Informe o valor do raio: ");
+    double raio = input.nextDouble();
+    
+    double area = raio * raio * PI;
+    
+    
+    System.out.println("O valor da area é: "+ area );
+    
+    
+    
+    }
+    
+}
